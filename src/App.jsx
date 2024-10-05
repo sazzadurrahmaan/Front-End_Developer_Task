f6427b9d1ec61e7f8cfc7ed8ec119cab528aea98
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-10-05 10:38:56 +0600

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* navbar component */}
       <NavBar/>
+      {/* hero component */}
+      <Hero/>
     </>
   )
 }
